2a29980f122c4ba1b61a91265b07aa2eb6a33c51
kramlj00 <kristinaramljak5@gmail.com>
2025-03-27 13:44:34 +0100

    Added image to TasteTheFlavors

--- components/taste-the-flavors.tsx
+++ components/taste-the-flavors.tsx
@@ -1,12 +1,24 @@
+import Image from 'next/image';
+
 const TasteTheFlavors = () => {
   return (
-    <div className="content-wrapper flex flex-col items-center gap-4">
-      <span className="text-neutral600 text-xl tracking-[0.5%]">
+    <div className="content-wrapper mb-[200px] flex flex-col items-center gap-4">
+      <span className="text-neutral600 text-center text-lg tracking-[0.5%] sm:text-xl">
         Savour authentic Turkish dishes in a warm setting.
       </span>
-      <h1 className="font-gambarino text-center text-9xl tracking-[-0.002em]">
+      <h1 className="font-gambarino text-center text-5xl tracking-[-0.002em] md:text-6xl lg:text-9xl">
         Taste the flavors of Turkey
       </h1>
+      <div className="relative h-[67vh] w-full">
+        <Image
+          src="/images/restaurant.png"
+          alt="Taste the flavors"
+          quality={100}
+          priority
+          fill
+          className="object-cover"
+        />
+      </div>
     </div>
   );
 };
