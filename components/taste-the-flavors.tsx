19f4a7162e1f851497e7933d20b6bfe588501def
kramlj00 <kristinaramljak5@gmail.com>
2025-03-27 13:44:54 +0100

    Updated TasteTheFlavors

--- components/taste-the-flavors.tsx
+++ components/taste-the-flavors.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 const TasteTheFlavors = () => {
   return (
     <div className="content-wrapper mb-[200px] flex flex-col items-center gap-4">
-      <span className="text-neutral600 text-center text-lg tracking-[0.5%] sm:text-xl">
+      <span className="text-center text-lg tracking-[0.5%] text-neutral600 sm:text-xl">
         Savour authentic Turkish dishes in a warm setting.
       </span>
-      <h1 className="font-gambarino text-center text-5xl tracking-[-0.002em] md:text-6xl lg:text-9xl">
+      <h1 className="text-center font-gambarino text-5xl tracking-[-0.002em] md:text-6xl lg:text-9xl">
         Taste the flavors of Turkey
       </h1>
       <div className="relative h-[67vh] w-full">
