00b4c7cdb3d446d7124728c042b417e9ce166e93
kramlj00 <kristinaramljak5@gmail.com>
2025-03-28 11:08:03 +0100

    Added responsiveness to our menu section

--- components/our-menu/our-menu.tsx
+++ components/our-menu/our-menu.tsx
@@ -9,31 +9,31 @@ import { menuPreviewItems } from './our-menu.const';
 
 const OurMenu = () => {
   return (
-    <div className="content-wrapper mt-[120px] flex flex-col gap-16">
+    <div className="content-wrapper mt-[120px] flex flex-col gap-12 md:gap-16">
       <SectionHeading title="Our menu" />
 
       <div className="relative grid grid-cols-1 md:grid-cols-2">
         <div className="absolute left-1/2 top-0 hidden h-full w-[1px] -translate-x-1/2 bg-foreground md:block" />
 
-        {menuPreviewItems.map((item, index) => (
+        {menuPreviewItems.map((item) => (
           <div
             key={item.title}
             className={cn(
-              'space-y-6 p-4 sm:p-[56px]',
-              index !== menuPreviewItems.length - 1 &&
-                'border-b border-foreground md:border-b-0',
+              'space-y-6 border-b border-foreground py-8 md:border-b-0 md:p-[56px]',
               item.className,
             )}
           >
-            <h2 className="font-gambarino text-heading-md">{item.title}</h2>
+            <h2 className="font-gambarino text-heading-sm md:text-heading-md">
+              {item.title}
+            </h2>
             {item.items.map((item, index) => (
               <div
                 key={index}
                 className="border-foreground/5 border-b pb-4 last:border-b-0"
               >
-                <div className="flex items-start justify-between">
+                <div className="flex items-start justify-between gap-2">
                   <div>
-                    <h3 className="flex items-center gap-2 font-medium">
+                    <h3 className="flex flex-wrap items-center gap-2 font-medium">
                       {item.name}
                       {item.dietary && <DietaryLabel type={item.dietary} />}
                     </h3>
