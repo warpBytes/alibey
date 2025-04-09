b47929b191527364974d1cf31bbc4262b06cd987
Boran Car <boran.car@gmail.com>
2025-04-09 13:27:36 +0200

    Use british spelling of flavours

--- components/taste-the-flavors.tsx
+++ components/taste-the-flavors.tsx
@@ -6,13 +6,13 @@ const TasteTheFlavors = () => {
           Savour authentic Turkish dishes in a warm setting.
         </span>
         <h1 className="text-center font-gambarino text-heading-xl tracking-[-0.002em] md:text-9xl">
-          Taste the flavors of Turkey
+          Taste the flavours of Turkey
         </h1>
       </div>
       <div className="relative h-[570px] w-full">
         <img
           src="/images/restaurant.png"
-          alt="Taste the flavors"
+          alt="Taste the flavours"
           className="object-cover"
           style={{ objectFit: 'cover', width: '100%', height: '100%' }}
         />
