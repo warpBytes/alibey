34e5817d3f5e777d8e178fda385071ce41635ddb
Dani283 <dani.machiedo28@gmail.com>
2025-04-10 10:55:10 +0200

    update about us section

--- components/about-us/about-us.tsx
+++ components/about-us/about-us.tsx
@@ -13,20 +13,20 @@ const AboutUs = () => {
       />
       <div className="content-wrapper mt-20 md:mt-0">
         <div className="flex w-full flex-wrap-reverse justify-between gap-10 md:gap-4">
-          <div className="flex max-w-[587px] flex-col gap-6">
-            <p className="text-center text-xl md:text-left md:text-2xl">
+          <div className="flex max-w-[708px] flex-col mx-auto gap-6 ">
+          <SectionHeading title="About us" className="mx-auto md:mx-0 md:mb-4" />
+            <p className="text-center text-xl  md:text-2xl">
               Our restaurant offers an authentic Turkish Mediterranean dining
               experience, blending traditional flavours with modern culinary
               techniques. Join us for an unforgettable meal!
             </p>
-            <BookNow className="mx-auto md:mx-0" />
+            <BookNow className="mx-auto " />
           </div>
-          <SectionHeading title="About us" className="mx-auto md:mx-0" />
         </div>
-        <div className="mt-[120px] flex flex-wrap justify-between gap-[120px] md:mt-[240px] md:gap-4">
-          <div className="mx-auto flex flex-col items-center justify-between gap-8 md:mx-0 md:items-start md:gap-4">
-            <span className="text-neutral600">Working hours</span>
-            <div className="flex flex-col items-center gap-6 text-center md:items-start md:gap-8 md:text-left">
+        <div className="mt-[120px] md:mt-[80px] flex flex-col justify-center gap-[120px] md:gap-[80px]">
+          <div className="mx-auto flex flex-col items-center justify-between gap-6 md:mx-0">
+            <span className="max-md:text-neutral600 md:text-2xl">Working hours:</span>
+            <div className="flex flex-col items-center gap-8 text-center">
               <span className="text-2xl uppercase md:text-heading-sm">
                 Monday to Sunday
               </span>
@@ -38,8 +38,7 @@ const AboutUs = () => {
               </div>
             </div>
           </div>
-
-          <div className="relative h-[361px] w-full md:h-[480px] md:w-[587px]">
+          <div className="relative h-[361px] w-full md:h-[570px]">
             <img
               src="/images/bar.png"
               alt="Bar"
