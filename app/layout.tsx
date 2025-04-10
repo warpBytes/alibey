3544e1514f389b69879cccf43c446236f855add3
Dani283 <dani.machiedo28@gmail.com>
2025-04-10 11:11:36 +0200

    add animated cursor

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navigation from '@/components/navigation';
 
 import { gambarino, switzer } from './fonts';
 import './globals.css';
+import AnimatedCursor from 'react-animated-cursor';
 
 export const metadata = metadataConfig;
 
@@ -18,6 +19,9 @@ export default function RootLayout({
       <body
         className={`${switzer.variable} ${gambarino.variable} bg-background antialiased`}
       >
+        <AnimatedCursor color='0, 0, 0'  innerSize={8} outerSize={24} innerScale={1} outerScale={1.5} outerAlpha={0.2} hasBlendMode={true} outerStyle={{
+          backgroundColor: '#ece8e1'
+        }}/>
         <Navigation />
         {children}
         <CookieBanner />
