5969b4d6d6a84ceb14947101e55f0fcb15bc885b
Boran Car <boran.car@gmail.com>
2025-10-18 22:07:12 -0500

    Add festive menu news with link to menu

--- components/news-and-updates/news-and-updates.const.ts
+++ components/news-and-updates/news-and-updates.const.ts
@@ -1,5 +1,11 @@
 export const newsAndUpdates = [
   {
+    title: 'Festive Menu',
+    description: 'Festive dining Ali Bey style.',
+    image: '/images/news/festive-menu.jpg',
+    link: '/festive-menu.pdf',
+  },
+  {
     title: 'Lunch Menu',
     description: 'Enjoy our exclusive lunch offerings at special prices.',
     image: '/images/news/lunch-menu.png',
--- components/news-and-updates/news-and-updates.tsx
+++ components/news-and-updates/news-and-updates.tsx
@@ -2,6 +2,14 @@ import BookNow from '../book-now';
 import SectionHeading from '../section-heading';
 import { newsAndUpdates } from './news-and-updates.const';
 
+interface OptionalLinkWrapperProps {
+  link?: string;
+  children: React.ReactNode;
+}
+
+const OptionalLinkWrapper: React.FC<OptionalLinkWrapperProps> = ({ link, children }: OptionalLinkWrapperProps) =>
+  (!!link ? <a href={link}>{children}</a> : <>{children}</>);
+
 const NewsAndUpdates = () => {
   return (
     <div className="content-wrapper mx-auto mb-[100px] flex flex-col gap-10 md:mb-[120px] md:gap-16">
@@ -9,24 +17,26 @@ const NewsAndUpdates = () => {
       <div className="mx-auto flex flex-wrap justify-center gap-10">
         {newsAndUpdates.map((item) => (
           <div key={item.title} className="flex max-w-[466px] flex-col gap-3">
-            <div className="relative h-[560px] w-full">
-              <img
-                src={item.image}
-                alt={item.title}
-                className="object-cover"
-                style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-              />
-            </div>
-            <span className="text-xl md:text-2xl">{item.title}</span>
-            <div className="h-[1px] w-full bg-foreground" />
-            <span className="text-lg text-neutral600 md:text-xl">
-              {item.description}
-            </span>
+            <OptionalLinkWrapper link={item.link}>
+              <div className="relative h-[560px] w-full">
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="object-cover"
+                  style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+                />
+              </div>
+              <span className="text-xl md:text-2xl">{item.title}</span>
+              <div className="h-[1px] w-full bg-foreground" />
+              <span className="text-lg text-neutral600 md:text-xl">
+                {item.description}
+              </span>
+            </OptionalLinkWrapper>
           </div>
         ))}
       </div>
       <BookNow className="mx-auto" />
-    </div>
+    </div >
   );
 };
 
