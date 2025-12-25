ce89c3f2f8ec80b6e63d67850c108be0f854b7eb
Boran Car <boran.car@gmail.com>
2025-12-25 23:14:53 +0100

    Remove festive menu

--- components/news-and-updates/news-and-updates.const.ts
+++ components/news-and-updates/news-and-updates.const.ts
@@ -3,21 +3,18 @@ export const newsAndUpdates = [
     title: 'New Year\'s Menu',
     description: 'New Year\'s Eve Celebration menu.',
     image: '/images/news/new-years-menu.jpg',
-  },
-  {
-    title: 'Festive Menu',
-    description: 'Festive dining Ali Bey style.',
-    image: '/images/news/festive-menu.jpg',
-    link: '/festive-menu.pdf',
+    link: '',
   },
   {
     title: 'Lunch Menu',
     description: 'Enjoy our exclusive lunch offerings at special prices.',
     image: '/images/news/lunch-menu.png',
+    link: '',
   },
   {
     title: 'Belly Dancer on Fridays and Saturdays',
     description: 'Experience an exciting live performance while dining.',
     image: '/images/news/dance-performance.png',
+    link: '',
   },
 ];
